1acc1a2cf756452261803f0249cb50ebca8cacca
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-09-10 12:25:28 +0100

    build login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,11 @@ module.exports = {
     extend: {
       colors: {
         primary: "#5E3B76",
+        dark: "#212529",
         my_yellow: "#FFD747",
         my_grey: "#f6f6f6",
+        green_bg: "#E9F6F7",
+        green_text: "#1B818C",
       },
     },
     fontFamily: {
