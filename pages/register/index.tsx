ef0d60c8015dc4c25d3d83c0ab0904cef54a8d13
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-09-10 14:41:04 +0100

    build register page

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,23 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { useRouter } from "next/router";
 
-import Header from "../components/Header";
-import Socials from "../components/Socials";
+import Header from "../../components/Header";
+import Socials from "../../components/Socials";
+import Footer from "../../components/Footer";
 
-const Home: NextPage = () => {
-  const router = useRouter();
-
-  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    e.preventDefault();
-    router.push("/register");
-  };
-
+const Register: NextPage = () => {
   return (
     <div>
       <Head>
         <title>Examine</title>
-        <meta name="description" content="Examine auth uI" />
+        <meta name="description" content="Examine auth UI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
@@ -27,10 +20,10 @@ const Home: NextPage = () => {
         md:px-10 md:pb-10"
         >
           <p className="font-bold text-xl text-dark font-secondary md:text-4xl">
-            Welcome back! 👋
+            Create your account
           </p>
           <p className="text-sm text-gray-400 md:text-lg">
-            {"Let's build someting great"}
+            It Includes a free 2 week trial!
           </p>
           <Socials />
           <div className="flex py-4 gap-4 justify-between items-end w-full">
@@ -39,6 +32,12 @@ const Home: NextPage = () => {
             <div className="w-1/2 h-4 border-t-2 border-grey-400"></div>{" "}
           </div>
           <form className="flex flex-col w-full gap-4 md:gap-6">
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="width-full h-9 border-2 border-gray-300 focus:outline-gray-400 pl-2 md:pl-3
+              md:h-11"
+            />
             <input
               type="email"
               placeholder="Email"
@@ -56,24 +55,14 @@ const Home: NextPage = () => {
               </span>
             </div>
             <button className="w-full bg-primary h-9 text-white rounded-sm md:h-14">
-              Login
+              Start your 2-week free trial!
             </button>
-            <div
-              className="p-8 bg-green_bg text-green_text border-2 border-dashed border-green_text 
-            rounded-sm text-sm text-center flex flex-col items-center justify-center cursor-pointer"
-              onClick={handleClick}
-            >
-              <p className="font-semibold ">Click here</p>
-              <p className="max-w-xs ">
-                to sign up for the complete Examine Membership. Includes a free
-                2 week trial!
-              </p>
-            </div>
           </form>
         </div>
       </main>
+      <Footer />
     </div>
   );
 };
 
-export default Home;
+export default Register;
